ddc9566bd2948cebebfde686474090af5dbf5d15
SowmyaSudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-14 22:52:29 +0530

    Update writing.js

--- theory/writing.js
+++ theory/writing.js
@@ -6,3 +6,7 @@ fs.writeFile('temp.txt', data, function(err, data){
     if (err) console.log(err);
     console.log("Successfully Written to File.");
 });
+
+output:
+PS C:\Users\Sowmya Sudhireddy\Desktop\week2\httpandhttps> node writing.js
+Successfully Written to File.
