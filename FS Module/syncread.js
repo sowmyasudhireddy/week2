5597684f3b3c4df4b8802ca2a6d42ed58dde7dcf
SowmyaSudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-14 23:50:41 +0530

    Update syncread.js

--- FS Module/syncread.js
+++ FS Module/syncread.js
@@ -8,3 +8,22 @@ var data = fs.readFileSync('readfile3.txt');
 console.log("Synchronous read: " + data.toString());
 
 console.log("Program Ended");
+
+output:
+
+PS C:\Users\Sowmya Sudhireddy\week2node> node syncread.js
+Synchronous read: reading file!!
+
+Synchronous read:
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+
+
+
+
+
+Synchronous read: reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+Program Ended
