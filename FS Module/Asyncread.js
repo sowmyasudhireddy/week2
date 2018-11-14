0094a09144b350d5747df9f802ecbadbf61952d3
SowmyaSudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-14 23:49:29 +0530

    Update Asyncread.js

--- FS Module/Asyncread.js
+++ FS Module/Asyncread.js
@@ -24,3 +24,35 @@ fs.readFile('readfile3.txt', function (err, data) {
 });
 
 console.log("sucessfull");
+
+output:
+
+
+PS C:\Users\Sowmya Sudhireddy\week2node> node Asyncread.js
+sucessfull
+Asynchronous read: reading file!!
+
+Asynchronous read: reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+Asynchronous read:
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+
+
+
+
+
+PS C:\Users\Sowmya Sudhireddy\week2node> node Asyncread.js
+sucessfull
+Asynchronous read: reading file!!
+
+Asynchronous read: reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+reading 3rd file............reading 3rd file............reading reading 3rd file............reading 3rd file............reading
+Asynchronous read:
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)reading second file :)
+
