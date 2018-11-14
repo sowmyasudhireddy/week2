b589bbe52a37a32a1d7770efc53d05fe2ccf9ac1
SowmyaSudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-14 22:51:37 +0530

    Update reading.js

--- theory/reading.js
+++ theory/reading.js
@@ -3,3 +3,8 @@ var fs = require('fs');
 fs.readFile('temp.txt', function(err, buf) {
   console.log(buf.toString());
 });
+
+
+output:
+PS C:\Users\Sowmya Sudhireddy\Desktop\week2\httpandhttps> node reading.js
+reading
